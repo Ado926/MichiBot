9d70d73e16e1a7c9b877bec7fc9497d0cc125261
Ado926 <adonaylester85@gmail.com>
2025-05-11 18:07:18 -0600

    Subida De Michi

---
+++ config.js
@@ -0,0 +1,102 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+// Staff De CrowBot
+global.owner = [
+   ['50493732693', 'Creador Wirk💛', true],
+   ['527205873392', 'CrowBot Soporte', true],
+   ['584148256527', 'Dev Diomar', true],
+   ['584120346669', 'Dev ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜', true],
+   ['584241836217', 'Dev Prak', true],
+   ['51968382008', 'Dev Darkcore', true],
+   ['5216633900512','Dev ʙʀᴀʏᴀɴᴏғᴄ', true],
+]
+
+global.creadorbot = [
+   ['50493732693', 'Wirk', true],
+]
+  
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = ['50368471855', '51920227615', '5219831715910', '573007796996', '50488198573', '51991055096', '5212731590195', '50557865603', '595975677765', '584245610338']
+
+global.prems = ['573215751237']
+
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.16' 
+global.vs = '2.2.0'
+global.nameqr = 'CrowBot-MF'
+global.namebot = 'CrowBot-MF'
+global.sessions = 'CrowSession'
+global.jadi = 'CrowJadiBot' 
+global.yukiJadibts = true // Activado
+global.ch = {
+ch1: '120363387375075395@newsletter',
+ch2: '120363368073378190@newsletter'
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = '≛ 𝐌𝐢𝐜𝐡𝐢  ✫'
+global.botname = '⚑🜲 𝐌𝐢𝐜𝐡𝐢 𝑩𝒐𝒕 ☘'
+global.wm = '⏤͟͟͞͞ ׄ ꒱ 𝘔𝘪𝘤𝘩𝘪 ୭'
+global.author = '✾ 𝒎𝒂𝒅𝒆 𝒃𝒚 𝒘𝒊𝒓𝒌 𓆪'
+global.dev = '✐ 𝖯᥆𝗐ᥱ𝗋ᥱძ ᑲᥡ ʂყα  𝖳ᥱᥲ𝗆 ☕︎︎'
+global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨𝐬 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
+global.namebot = '𝘔𝘪𝘤𝘩𝘪 𝐬𝐲𝐚'
+global.textbot = `「  𝘔𝘪𝘤𝘩𝘪 νя 」`
+global.vs = '2.2.0'
+global.publi = '✰ 𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.imagen1 = fs.readFileSync('./media/menus/Menu.jpg')
+global.imagen2 = fs.readFileSync('./media/menus/Menu2.jpg')
+global.imagen3 = fs.readFileSync('./media/menus/Menu3.jpg')
+global.welcome = fs.readFileSync('./media/welcome.jpg')
+global.adios = fs.readFileSync('./media/adios.jpg')
+global.catalogo = fs.readFileSync('./media/catalogo.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.repobot = 'https://github.com/WillZek/CrowBot-ST'
+global.grupo = 'https://chat.whatsapp.com/B7POPrlJiZf7UWHJKvzVkx'
+global.gteam = 'https://chat.whatsapp.com/EpkR7XS0hrMDYk89yw57HZ'
+global.gsupport = 'https://chat.whatsapp.com/K5GLB5CXp3iAl4XsDzav1W'
+global.comu = 'https://chat.whatsapp.com/Bgfb6Ag13BDCvwzOtldlCm'
+global.channel = 'https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W'
+global.insta = 'https://www.instagram.com/crowbot-wa'
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '【✫𝚃𝙴𝙰𝙼 𝐒𝐘𝐀 ✫】', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment        
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
---
+++ plugins/descargas-soundcloud.js
@@ -0,0 +1,38 @@
+// By WillZek >> https://github.com/WillZek
+
+import fetch from 'node-fetch';
+import fg from 'senna-fg';
+
+let handler = async(m, { conn, usedPrefix, command, text }) => {
+
+if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command} crow edits`);
+
+try {
+let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+
+let results = api.data[0];
+
+let txt = `🥞 *Mira lo que encontré:*\n\n*${results.title}*\n\n⏱️ \`Dura ${results.duration} minutos\`\n🔗 \`Aquí tienes el enlace: ${results.url}\`\n\n> ${dev}`;
+
+let img = results.image;
+
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
+
+/* let api2 = await(await fetch(`https://api.neoxr.eu/api/youtube?url=${results.url}&type=audio&quality=128kbps&apikey=GataDios`)).json();
+
+if (!api2?.data.url) return m.reply('No Se  Encontraron Resultados');
+*/
+
+let api2 = await(await fetch(`https://api.vreden.my.id/api/ytmp3?url=${results.url}`)).json();
+
+conn.sendMessage(m.chat, { audio: { url: api2.result.download.url }, mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
+
+} catch (e) {
+m.reply(`*No Encontramos Resultados Para Tu Búsqueda* ${e.message}\n`);
+m.react('✖️');
+  }
+}
+
+handler.command = ['play', 'paudio'];
+
+export default handler
